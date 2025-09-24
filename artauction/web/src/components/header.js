74adcfce6d96013d0770f4ead5674736c5ffffb8
AsmaMaryam25 <s230716@dtu.dk>
2025-09-24 20:50:26 +0200

    Header created with logind and logo"

--- artauction/web/src/components/header.js
+++ artauction/web/src/components/header.js
@@ -1,17 +1,34 @@
 import React from "react";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import logo  from '../assets/logo.png';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 const Header = () => {
   return (
-    <AppBar position="static" color="transparent" sx={{ paddingY: 0.5 }}>
+    <AppBar position="static" sx={{ backgroundColor: "#e5ebe4", color: "#44543f", paddingY: 0.5 }}>
       <Toolbar>
         <img src={logo} alt="Art Auction Logo" style={{ height: 60 }} />
-        <Typography variant="h6"></Typography>
-        <Box sx={{ flexGrow: 1 }} />
-        <Button color="inherit">Login</Button>
+        <Box sx={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginLeft: "auto" ,
+            padding: "3px 6px",
+            borderRadius: "8px",
+            transition: "all 0.3s ease",
+          "&:hover .login-icon": {
+            color: "#2e7516"
+            },
+         "&:hover .login-text": {
+            color: "#2e7516"
+            },
+        }}
+            onClick={() => alert('Login functionality to be implemented')}
+             >
+          <AccountCircle className="login-icon" sx={{ fontSize: 25 }} />
+        <Typography className="login-text" sx={{ fontSize: 13 }} variant="body1">Login</Typography>
+        </Box>
       </Toolbar>
     </AppBar>
-
   );
 };
 
