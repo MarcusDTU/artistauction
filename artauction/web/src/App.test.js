7b203c1d1d7d33c0b0f3d1e177652ac4f4109d20
Marcus <s195080@dtu.dk>
2025-10-30 10:55:05 +0100

    added test to test the new button

--- artauction/web/src/App.test.js
+++ artauction/web/src/App.test.js
@@ -1,11 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
+
+beforeEach(() => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+});
+
+afterEach(() => {
+    window.alert.mockRestore();
+});
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  let button =screen.getByRole("button");
-  expect(button).toHaveTextContent("Login");
 });
+test('renders bid input and submit button', () => {
+    render(<App />);
+    const input = screen.getByLabelText(/bid amount/i);
+    const button = screen.getByRole('button', { name: /submit/i });
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+});
+test('submit valid bid', () => {
+    render(<App />);
+    const input = screen.getByLabelText(/bid amount/i);
+    const button = screen.getByRole('button', { name: /submit/i });
+    userEvent.type(input, '100');
+    userEvent.click(button);
+    expect(window.alert).toHaveBeenCalledWith('Bid submitted: 100');
+})
+
+
 
