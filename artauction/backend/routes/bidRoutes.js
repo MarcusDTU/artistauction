d260d5c665714692a5f42cab0453c46bd237fdb3
Marcus <s195080@dtu.dk>
2025-11-20 14:22:58 +0100

    add route to fetch bid price by bid ID

--- artauction/backend/routes/bidRoutes.js
+++ artauction/backend/routes/bidRoutes.js
@@ -1,9 +1,10 @@
 // routes/bidRoutes.js
 import express from 'express';
-import { fetchAllBids } from '../controller/bidController.js';
+import { fetchAllBids, fetchBidPrice } from '../controller/bidController.js';
 
 const router = express.Router();
 
 router.get('/', fetchAllBids);
+router.get('/:bidId', fetchBidPrice);
 
 export default router;
