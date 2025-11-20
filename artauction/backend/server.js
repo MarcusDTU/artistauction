15813b0dd4ce6ef11d292601696318eb68646f50
Marcus <s195080@dtu.dk>
2025-11-20 12:55:52 +0100

    add server.js to set up Express server with bid routes

---
+++ artauction/backend/server.js
@@ -0,0 +1,36 @@
+import {supabase} from "./dbConfig.js";
+import express from "express";
+import cors from "cors";
+import bidRoutes from "./routes/bidRoutes.js";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/bid', bidRoutes)
+
+// Get all bids (most recent first)
+/*app.get('/bid', async (req, res) => {
+    try {
+        const {data, error} = await supabase
+            .from('Bid')
+            .select('*');
+        if (error){
+            return res.status(500).json({error: error.message});
+        }
+        return res.json(data);
+    } catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+});
+*/
+
+
+
+app.get('/',(req, res) => {
+    return res.json("Server is running");
+})
+
+app.listen(8081, () => {
+    console.log("listing")
+})
